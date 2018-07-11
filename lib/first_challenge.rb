d10138b545fcc815f63e0abb8628aa46a5070cd4
Isiah VanBrackle <squarepheonix@gmail.com>
2018-07-11 02:57:30 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -12,11 +12,27 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
   #your code here
+   contacts.each do |person, data|
+     if person == "Freddy Mercury"
 
-
+   data.each do |what, confused|
+     if what == :favorite_icecream_flavors
+       confused.shift
+     end
+   end
+ end
+end
+  #this felt great i spent about 30 mins in repl.it just figuring out what level
+  #of the code i was in, once i did i actually went to far and tried to .shift the
+  #string "strawberry". I dailed it back then tried what.shift, the error is what
+  #made me realize i was trying to .shift the symbol :favorite_icecream_flavors
+  #from there it was clear i'd see success if i just .shifted confused. // my
+  #original plan was
+  # - error = contacts["Freddy Mercury"][:favorite_icecream_flavors]
+  #  error.shift -
+  #this works as far as getting rid of the "strawberry" goes, but the lab is
+  #specifically asking me to iterate down to that particular level
   #remember to return your newly altered contacts hash!
   contacts
 end
-
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,6 +8,26 @@ def second_challenge
 
   #code your solution here!
 
-  
-
-end
+    # well then this was simpler, this is the first thing i tried... welp I take
+    # that back...
+    # - groceries.each do |category|
+    # puts category -
+    # was the first thing i tried in repl.it and it put what i wanted. However
+    # when i ran the test on my terminal i noticed i needed the .values method...
+    # a few mins of research later and i'm ready to present my the real answer
+    # MWAHAHAHAHAHAHAHA!!!!!
+    list = []
+    groceries.each do |categories, value|
+       list << value
+    end
+    # ok... that was not a few mins of work... that was hours. Honestly I didn't
+    # solve this. All of the above i figured out. but calling list after just
+    # returns a nested array. Naturally i looked up how to to make a nested array
+    # == a single array, and got nothing remotely close to what i was looking for
+    # so i spent an hour trying different tricks to see what would work, before
+    # finally flipping the table and just checking out the solution over on github
+    # only to see .flatten... OF COURSE IT'S .FLATTEN!!! ugh can't beleive i forgot
+    # that method... Oh and icing on the cake when i searched "how to to make a nested array
+    # == a single array" the first result 'anecdotally' mentions flatten...
+     list.flatten
+   end
